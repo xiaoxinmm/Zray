4b63789797a74b4b4704aef9e9dedaedd30005e3
Russell Lee <xiaoxinmm@users.noreply.github.com>
2026-03-05 13:34:56 +0000

    fix: ExitEventArgs命名空间

--- gui/ZRayClient/App.xaml.cs
+++ gui/ZRayClient/App.xaml.cs
@@ -4,7 +4,7 @@ namespace ZRayClient
 {
     public partial class App : System.Windows.Application
     {
-        protected override void OnExit(ExitEventArgs e)
+        protected override void OnExit(System.Windows.ExitEventArgs e)
         {
             // 兜底：杀所有残留的 zray-client 进程
             try
